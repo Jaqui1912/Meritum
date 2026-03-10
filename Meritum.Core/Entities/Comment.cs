6bffc6c16b5611d140d8934e2c095b0eaa3b7a91
Navis2006 <angel.er.nava.sa@gmail.com>
2026-03-09 22:47:10 -0600

    comments

--- Meritum.Core/Entities/Comment.cs
+++ Meritum.Core/Entities/Comment.cs
@@ -7,7 +7,7 @@ public class Comment
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; } = null!;
+    public string? Id { get; set; }
 
     [BsonElement("projectId")]
     public string ProjectId { get; set; } = null!;
