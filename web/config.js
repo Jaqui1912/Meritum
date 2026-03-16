2f903b2320e6752c96bac614db457167c3c3ab1e
Navis2006 <angel.er.nava.sa@gmail.com>
2026-03-16 16:23:28 -0600

    nueva_conexion

--- web/config.js
+++ web/config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
 
     //API_BASE_URL: 'http://localhost:5227', // <--- Descomenta para probar localmente
-    API_BASE_URL: 'https://meritum.onrender.com' // <--- Descomenta para producción en Render
+    API_BASE_URL: 'https://gorgeous-dominica-proyectosutm-caadf350.koyeb.app' // <--- Descomenta para producción en Koyeb
 };
